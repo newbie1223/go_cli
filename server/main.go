32807ea114768413afd434f17a67765a1c955699
newbie1223 <taiti7919@gmail.com>
2024-03-24 07:04:09 +0900

    go get tview

--- server/main.go
+++ server/main.go
@@ -1,25 +1,42 @@
 package main
 
-import "fmt"
-
-type Walker struct {
-	Name string
-}
-
-func (w *Walker) Walk() { fmt.Printf("%s is walking\n", w.Name) }
-
-type Runner struct {
-	Walker
-}
-
-func NewRunner(name string) *Runner {
-	return &Runner{Walker{Name: name}}
-}
-
-func (r *Runner) Run() { fmt.Printf("%s is running\n", r.Name) }
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 func main() {
-	runner := NewRunner("John")
-	runner.Walk()
-	runner.Run()
+	app := tview.NewApplication()
+
+	textView := tview.NewTextView()
+	textView.SetTitle("gwork")
+	textView.SetBorder(true)
+
+	inputField := tview.NewInputField()
+	inputField.SetLabel("@user: ")
+	inputField.SetTitle("chat").SetBorder(true)
+	inputField.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEnter:
+			textView.SetText(textView.GetText(true) + inputField.GetText() + "\n")
+			inputField.SetText("")
+			return nil
+		}
+		return event
+	})
+
+	// inputField.SetChangedFunc(func(text string) { // inputFieldの入力内容が変更される度に実行
+	// 	textView.SetText(textView.GetText(true) + inputField.GetText() + "\n")
+	// 	inputField.SetText("")
+	// 	return
+	// })
+
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow).
+		AddItem(inputField, 3, 0, true).
+		AddItem(textView, 0, 1, false)
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
 }
