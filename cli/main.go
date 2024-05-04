f96db983728ed3e979cf3d1ca014fefaf8e5d47c
newbie1223 <taiti7919@gmail.com>
2024-05-04 14:42:19 +0900

    remake server main

---
+++ cli/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+	// "github.com/mattn/go-runewidth"
+	"github.com/rivo/tview"
+)
+
+type ui struct {
+	app     *tview.Application
+	curTime *tview.TableCell
+}
+
+const refreshInterval = 500 * time.Millisecond
+
+func currentTimeString() string {
+	t := time.Now()
+	return fmt.Sprintf(t.Format("15:04:05"))
+}
+
+func updateTime(u *ui) {
+	for {
+		time.Sleep(refreshInterval)
+		u.app.QueueUpdateDraw(func() {
+			u.curTime.SetText(currentTimeString())
+		})
+	}
+}
+
+func createCommandList() (commandList *tview.List) {
+	commandList = tview.NewList()
+	commandList.SetBorder(true).SetTitle("Command")
+	return commandList
+}
+
+func createInfoPanel(app *tview.Application, u *ui) (infoPanel *tview.Grid) {
+
+	infoTable := tview.NewTable()
+	infoTable.SetBorder(true).SetTitle("Information")
+
+	cnt := 0
+	infoTable.SetCellSimple(cnt, 0, "Time:")
+	infoTable.GetCell(cnt, 0).SetAlign(tview.AlignRight)
+	u.curTime = tview.NewTableCell("0")
+	infoTable.SetCell(cnt, 1, u.curTime)
+	cnt++
+
+	// infoPanel = tview.NewFlex().SetDirection(tview.FlexRow).AddItem(infoTable, 0, 1, false)
+	infoPanel = tview.NewGrid().SetRows(-1).SetColumns(-1).AddItem(infoTable, 0, 0, 1, 1, 0, 0, false)
+
+	return infoPanel
+}
+
+func createLayout(cList tview.Primitive, recvPanel tview.Primitive, tPanel tview.Primitive) (layout *tview.Flex) {
+	// bodyLayout := tview.NewFlex().SetDirection(tview.FlexColumn).
+	// 	AddItem(cList, 20, 1, true).
+	// 	AddItem(recvPanel, 0, 1, false).
+	// 	AddItem(tPanel, 0, 1, false)
+
+	bodyLayout := tview.NewGrid().SetRows(3, 1, 3).SetColumns(3, 3, 4).
+		SetBorders(true).
+		AddItem(cList, 0, 0, 1, 3, 0, 0, false).
+		AddItem(recvPanel, 2, 0, 1, 3, 0, 0, false).
+		AddItem(tPanel, 1, 1, 3, 2, 0, 0, false)
+
+	layout = tview.NewFlex().SetDirection(tview.FlexRow).AddItem(bodyLayout, 0, 1, true)
+	// layout = tview.NewGrid().SetRows(-1).SetColumns(-1).AddItem(bodyLayout, 0, 0, 1, 1, 0, 0, true)
+
+	// layout = tview.NewFlex().SetDirection(tview.FlexRow).AddItem(bodyLayout, 0, 1, true)
+	// bodyLayout := tview.NewGrid().AddItem(cList, 0, 0, 0, 0, 0, 0, true).AddItem(recvPanel, 0, 0, 1, 2, 100, 0, true).AddItem(tPanel, 1, 1, 3, 2, 300, 0, true)
+	// layout = tview.NewFlex().SetDirection(tview.FlexRow).AddItem(bodyLayout, 0, 1, true)
+
+	return layout
+}
+
+// func createModalForm(pages *tview.Pages, form tview.Primitive, height int, width int) tview.Primitive {
+// 	modal := tview.NewFlex().SetDirection(tview.FlexColumn).
+// 		AddItem(nil, 0, 1, false).
+// 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+// 			AddItem(nil, 0, 1, false).
+// 			AddItem(form, height, 1, true).
+// 			AddItem(nil, 0, 1, false), width, 1, true).
+// 		AddItem(nil, 0, 1, false)
+// 	return modal
+// }
+
+func createTextViewPanel(app *tview.Application, title string) (textPanel *tview.TextView) {
+	textPanel = tview.NewTextView()
+	textPanel.SetTitle(title)
+	textPanel.SetBorder(true)
+	return textPanel
+}
+
+func createTextInputPanel(app *tview.Application, title string) (inputPanel *tview.Grid) {
+
+	textView := tview.NewTextView()
+	textView.SetTitle("gwork")
+	textView.SetBorder(true)
+
+	inputField := tview.NewInputField()
+	inputField.SetLabel("@user: ")
+	inputField.SetTitle("chat").SetBorder(true)
+	inputField.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEnter:
+			textView.SetText(textView.GetText(true) + inputField.GetText() + "\n")
+			inputField.SetText("")
+			return nil
+		}
+		return event
+	})
+
+	// inputPanel = tview.NewFlex().SetDirection(tview.FlexRow).AddItem(inputField, 3, 0, true).AddItem(textView, 0, 1, false)
+	inputPanel = tview.NewGrid().SetRows(-1, -3).SetColumns(-1).AddItem(textView, 0, 0, 1, 1, 0, 0, false).AddItem(inputField, 1, 0, 1, 1, 0, 0, false)
+
+	return inputPanel
+
+}
+
+// func testCommand(pages *tview.Pages) func() {
+// 	return func() {
+// 		cancelFunc := func() {
+// 			pages.SwitchToPage("main")
+// 			pages.RemovePage("modal")
+// 		}
+
+// 		onFunc := func() {
+// 			pages.SwitchToPage("main")
+// 			pages.RemovePage("modal")
+// 		}
+
+// 		form := tview.NewForm()
+// 		form.AddButton("ON", onFunc)
+// 		form.AddButton("Cancel", cancelFunc)
+// 		form.SetCancelFunc(cancelFunc)
+// 		form.SetButtonsAlign(tview.AlignCenter)
+// 		form.SetBorder(true).SetTitle("Test")
+// 		modal := createModalForm(pages, form, 13, 55)
+// 		pages.AddPage("modal", modal, true, true)
+// 	}
+// }
+
+func createApplication() (app *tview.Application) {
+	app = tview.NewApplication()
+	pages := tview.NewPages()
+
+	ui := &ui{}
+	ui.app = app
+	infoPanel := createInfoPanel(app, ui)
+	textPanel := createTextViewPanel(app, "gwork")
+	// inputPanel := createTextViewPanel(app, "chat",textPanel)
+
+	commandList := createCommandList()
+	// commandList.AddItem("Test", "", 'p', testCommand(pages))
+	commandList.AddItem("Test", "", 'p', func() {
+		textPanel.SetText(textPanel.GetText(true) + "test\n")
+	})
+	commandList.AddItem("Clear", "", 'c', func() {
+		textPanel.SetText("")
+	})
+	commandList.AddItem("Quit", "", 'q', func() {
+		app.Stop()
+	})
+	commandList.AddItem("Input", "", 'i', func() {
+		inputPanel := createTextInputPanel(app, "chat")
+		// pages.AddPage("input", inputPanel, true, true)
+		pages.AddPage("main", inputPanel, true, true)
+	})
+	layout := createLayout(commandList, infoPanel, textPanel)
+	pages.AddPage("main", layout, true, true)
+
+	go updateTime(ui)
+	app.SetRoot(pages, true)
+	return app
+}
+
+func main() {
+	// runewidth.DefaultCondition = &runewidth.Condition{EastAsianWidth: false}
+
+	app := createApplication()
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+	if err := app.EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,6 +1,102 @@
+// Demo code for the Grid primitive.
+// package main
+
+// import (
+// 	"github.com/gdamore/tcell/v2"
+// 	"github.com/rivo/tview"
+// )
+
+// func main() {
+// current, err := os.Getwd()
+// if err != nil {
+// 	fmt.Println(err)
+// }
+// fmt.Println(current)
+// sc := bufio.NewScanner(os.Stdin)
+// for sc.Scan() {
+// 	input := sc.Text()
+// 	inputs := strings.Split(input, " ")
+// 	switch inputs[0] {
+// 	case "exit":
+// 		return
+// 	case "ls":
+// 		f, err := os.Open(current)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		files, err := f.Readdir(-1)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		for _, file := range files {
+// 			fmt.Println(file.Name())
+// 		}
+// 	case "cd":
+// 		// 実際にディレクトリが存在するかどうかはチェックする
+// 		f, err := os.Open(current)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		files, err := f.Readdir(-1)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		exist := false
+// 		for _, file := range files {
+// 			if file.Name() == inputs[1] {
+// 				if file.IsDir() {
+// 					current = current + "/" + inputs[1]
+// 				} else {
+// 					fmt.Println("Not a directory")
+// 				}
+// 				exist = true
+// 				break
+// 			}
+// 		}
+// 		if !exist {
+// 			fmt.Println("No such file or directory")
+// 		}
+// 	default:
+// 		fmt.Println("Unknown command")
+// 	}
+// 	fmt.Print("current> ", current, "\n")
+// }
+// 	newPrimitive := func(text string) tview.Primitive {
+// 		return tview.NewTextView().
+// 			SetTextAlign(tview.AlignCenter).
+// 			SetText(text)
+// 	}
+// 	menu := newPrimitive("Menu")
+// 	main := newPrimitive("Main content")
+// 	sideBar := newPrimitive("Side Bar")
+
+// 	grid := tview.NewGrid().
+// 		SetRows(-3, -1, -3).
+// 		SetColumns(-33, -33, -34).
+// 		SetBorders(true).
+// 		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
+// 		AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
+
+// 	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+// 	grid.AddItem(menu, 0, 0, 0, 0, 0, 0, false).
+// 		AddItem(main, 1, 0, 1, 3, 0, 0, false).
+// 		AddItem(sideBar, 0, 0, 0, 0, 0, 0, false)
+
+// 	// Layout for screens wider than 100 cells.
+// 	grid.AddItem(menu, 1, 0, 1, 1, 0, 100, false).
+// 		AddItem(main, 1, 1, 1, 1, 0, 100, false).
+// 		AddItem(sideBar, 1, 2, 1, 1, 0, 100, false)
+
+// 	if err := tview.NewApplication().SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
+// 		panic(err)
+// 	}
+// }
+
 package main
 
 import (
+	"fmt"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
@@ -8,33 +104,35 @@ import (
 func main() {
 	app := tview.NewApplication()
 
-	textView := tview.NewTextView()
-	textView.SetTitle("gwork")
-	textView.SetBorder(true)
+	// チャット履歴を表示するためのTextViewを作成します。
+	historyView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
 
+	// ユーザーがメッセージを入力するためのInputFieldを作成します。
 	inputField := tview.NewInputField()
-	inputField.SetLabel("@user: ")
-	inputField.SetTitle("chat").SetBorder(true)
+	inputField.SetLabel("メッセージ: ")
+	inputField.SetFieldWidth(0)
 	inputField.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyEnter:
-			textView.SetText(textView.GetText(true) + inputField.GetText() + "\n")
+		if event.Key() == tcell.KeyEnter {
+			// メッセージを取得してクリアします。
+			message := inputField.GetText()
 			inputField.SetText("")
-			return nil
+
+			// メッセージを履歴に追加します。
+			fmt.Fprintf(historyView, "あなた: %s\n", message)
 		}
 		return event
 	})
 
-	// inputField.SetChangedFunc(func(text string) { // inputFieldの入力内容が変更される度に実行
-	// 	textView.SetText(textView.GetText(true) + inputField.GetText() + "\n")
-	// 	inputField.SetText("")
-	// 	return
-	// })
-
-	flex := tview.NewFlex()
-	flex.SetDirection(tview.FlexRow).
-		AddItem(inputField, 3, 0, true).
-		AddItem(textView, 0, 1, false)
+	// レイアウトを作成します。
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(historyView, 0, 1, false).
+		AddItem(inputField, 1, 1, true)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
